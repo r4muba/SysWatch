704e0d116e7ebfcdefebb901b94c96be44b50bf7
Logan <logangd2006@gmail.com>
2026-05-30 19:05:41 -0600

    added memory data

--- src/systemData.py
+++ src/systemData.py
@@ -1,3 +1,12 @@
+import psutil
+
 class SystemData:
     def __init__(self):
-        ...
+        self.getRamData()
+    
+    def getRamData(self):
+        '''
+        https://psutil.readthedocs.io/stable/#psutil.virtual_memory
+        '''
+        return psutil.virtual_memory()
+        
