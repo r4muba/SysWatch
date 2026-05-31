cf48d183cb71c29b16c91480b71f61cffeab2fc4
Rogelio G. <rogelioguerreo14@gmail.com>
2026-05-30 21:57:07 -0600

    Tabl and graph Frams addd

--- src/gui/panels/processPanel.py
+++ src/gui/panels/processPanel.py
@@ -11,6 +11,12 @@ class ProcessPanel(tb.Frame):
 
         self._build()
 
+    def change(self, frame_1, frame_2):
+        frame_1.pack_forget()
+        frame_2.pack_forget()
+
+        frame_1.pack(fill="both", expand =True)
+
     def _build(self):
         title = tb.Label (
             self,
@@ -20,29 +26,48 @@ class ProcessPanel(tb.Frame):
         )
         title.pack(anchor="center", padx=30, pady=30)
 
+        # Declaration
+
         aux_frame = tb.Frame(self, bootstyle="danger")
         aux_frame.pack(fill="both", expand =True)
+
+        left_frame = tb.Frame(aux_frame, bootstyle="primary")
+        right_frame = tb.Frame(aux_frame, bootstyle="secondary")
+
+        table_frame = tb.Frame(right_frame,bootstyle="dark")
+        graph_frame = tb.Frame(right_frame,bootstyle="light")
+
+        table_btn = tb.Button(
+            left_frame, 
+            text="Tabla", 
+            bootstyle="success", 
+            command=lambda: self.change(table_frame,graph_frame)
+        )
+
+        graph_btn = tb.Button(
+            left_frame, 
+            text="Grafica", 
+            bootstyle="danger",
+            command=lambda: self.change(graph_frame,table_frame)
+        )
+
+        # Column configure
 
         aux_frame.columnconfigure(0, weight=2)
         aux_frame.columnconfigure(1, weight=6)
         aux_frame.rowconfigure(0, weight=1)
 
-        left_frame = tb.Frame(aux_frame, bootstyle="primary")
         left_frame.columnconfigure(0, weight=1)
         
         for i in range(4):  
             left_frame.rowconfigure(i, weight=1)
 
 
-        right_frame = tb.Frame(aux_frame, bootstyle="secondary")
 
         right_frame.grid(row=0, column=1, sticky="nsew")
         left_frame.grid(row=0, column=0, sticky="nsew")
 
-        table_btn = tb.Button(left_frame, text="Tabla", bootstyle="success")
         table_btn.grid(row=1, column=0, sticky="nsew")
-
-        graph_btn = tb.Button(left_frame, text="Grafica", bootstyle="danger")
         graph_btn.grid(row=2, column=0, sticky="nsew")
 
 
