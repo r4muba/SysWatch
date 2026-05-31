8dac3927c0eba8f2e528fdc35be15c156fe6e8dd
AxelZap <ironaxel.az@gmail.com>
2026-05-30 20:18:55 -0600

    logan me dijo que haga push

--- src/gui/panels/memoryPanel.py
+++ src/gui/panels/memoryPanel.py
@@ -2,13 +2,16 @@ import ttkbootstrap as tb
 import threading
 
 class MemoryPanel(tb.Frame):
+    
     _MB = 1024 * 1024
-    def __init__(self, frame, systemData):
+
+    def __init__(self, frame, systemData, threadController):
         super().__init__(
             frame,
             style="success"
         )
         self._systemData = systemData
+        self._threadController = threadController
         self.build()
 
     def build(self):
@@ -19,6 +22,14 @@ class MemoryPanel(tb.Frame):
             bootstyle="warning"
         )
         title.pack(anchor="w", padx=30, pady=20)
+
+        totalText = tb.Label(
+            self,
+            text="TOTAL",
+            font=("Helvetica", 24, "bold"),
+            bootstyle="warning"
+        )
+        totalText.place(x=100, y=100)
 
         memoryData = self._systemData.getMemoryData()
 
@@ -33,16 +44,27 @@ class MemoryPanel(tb.Frame):
             bootstyle="success",    
             interactive=False       # El usuario no lo puede mover con el mouse
         )
-        self.memory_available.pack(pady=100)
+        self.memory_available.place(x=100,y=200)
 
-        thread = threading.Thread(target=self.actualizar_ram)
-        thread.start()
+        self.memory_available = tb.Meter(
+            master=self,
+            metersize=200,
+            amountused=0,           # Empezamos en 0%
+            amounttotal=memoryData.total/self._MB,        # El total
+            metertype="semi",       # Estilo velocímetro
+            textright="MB",    
+            subtext="uso RAM",    
+            bootstyle="success",    
+            interactive=False       # El usuario no lo puede mover con el mouse
+        )
+        self.memory_available.place(x=100,y=200)
+
+        self._threadController.createThread(self.actualizar_ram)
 
     # 2. Función para actualizar el valor dinámicamente
     def actualizar_ram(self):
-        while True:
-            memoryData = self._systemData.getMemoryData()
+        memoryData = self._systemData.getMemoryData()
 
-            used_mb = memoryData.used / self._MB
+        used_mb = memoryData.used / self._MB
 
-            self.memory_available.configure(amountused=used_mb)
+        self.memory_available.configure(amountused=used_mb)
